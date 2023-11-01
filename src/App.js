801868f515faca76a1dc910e19a442721bf2a79f
Jaseem <muhammedjaseemvk96@gmail.com>
2023-11-01 20:50:51 +0530

    added scrollbar,set fixed height,fixed add-to-do ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Todo from './Todo';
 
 
@@ -18,7 +18,7 @@ function App() {
       return
     }
     const newList = [...toDoList, { task: inputTodo, status: false }];
-    localStorage.setItem('todos',JSON.stringify(newList));
+    localStorage.setItem('todos', JSON.stringify(newList));
     setToDoList(newList);
     setInputTodo('');
   }
@@ -27,7 +27,7 @@ function App() {
     const newList = toDoList.filter((toDo, i) => {
       return index !== i
     });
-    localStorage.setItem('todos',JSON.stringify(newList));
+    localStorage.setItem('todos', JSON.stringify(newList));
     setToDoList(newList);
   }
 
@@ -38,12 +38,12 @@ function App() {
       }
       return toDo;
     });
-    localStorage.setItem('todos',JSON.stringify(newList));
+    localStorage.setItem('todos', JSON.stringify(newList));
     setToDoList(newList);
   }
 
   useEffect(() => {
-    if(localStorage.getItem('todos')){
+    if (localStorage.getItem('todos')) {
       setToDoList(JSON.parse(localStorage.getItem('todos')))
     }
   }, [])
@@ -60,28 +60,33 @@ function App() {
     setDoneToDos(completeToDos.length);
     setActiveToDos(notCompleteToDos.length);
   }, [toDoList])
-  
+
 
 
 
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen text-white bg-slate-900'>
-      <div className="flex flex-col bg-slate-800 gap-3 p-2 rounded-md text-center ">
-        <div>
-          <h1>ToDo App</h1>
-          <input type="text" className='rounded-md bg-slate-500 px-1' value={inputTodo} onChange={handleOnChange} />
-          <button className='rounded-md bg-yellow-500 px-1' onClick={handleAdd} >Add</button>
+    <div className='flex justify-center items-center w-screen h-screen text-white bg-slate-900'>
+      <div className="flex flex-col bg-slate-800 gap-3 p-5 rounded-md text-center h-3/4">
+        <div className='flex flex-col gap-2'>
+          <h1 className='font-bold'>ToDo App</h1>
+          <div className="flex flex-row">
+            <input type="text" className='rounded-l-md bg-slate-500 px-3' name="todo" value={inputTodo} onChange={handleOnChange} />
+            <button className='rounded-r-md bg-yellow-500 px-3 py-1' name="addButoon" onClick={handleAdd} >Add</button>
+          </div>
         </div>
-        {toDoList.length > 0 ? (<div className='flex flex-col rounded-md bg-slate-500 p-3 gap-2'>
-          {doneToDos} done {activeToDos} to go
-          {
-            toDoList.map((toDoElement, index) => {
-              return <Todo key={index} todoItem={toDoElement} handleDelete={() => handleDelete(index)} handleStatus={() => handleStatus(index)} />
-            })
-          }
-        </div>)
-          : null}
+        <div className='flex flex-col rounded-md bg-slate-500 p-3 gap-2 h-full overflow-auto'>
+          {toDoList.length > 0 ? (<>
+            {doneToDos} done {activeToDos} to go
+            {
+              toDoList.map((toDoElement, index) => {
+                return <Todo key={index} todoItem={toDoElement} handleDelete={() => handleDelete(index)} handleStatus={() => handleStatus(index)} />
+              })
+            }
+          </>
+          )
+            : null}
+        </div>
       </div>
 
     </div>
--- src/Todo.js
+++ src/Todo.js
@@ -5,7 +5,7 @@ function Todo(props) {
 
     return (
         <div className={`${props.todoItem.status?'line-through ':''} flex justify-between items-center rounded bg-yellow-500 px-2 py-1  w-full`}>
-            <input type="checkbox" checked={props.todoItem.status} onClick={props.handleStatus} />
+            <input type="checkbox" checked={props.todoItem.status} name="checkbox" onChange={props.handleStatus} />
             {props.todoItem.task}
             <FaRegTrashAlt onClick={props.handleDelete} />
         </div>
